29074f649a4c19d7a6364b524692987ede831a99
usuario <mlungut2812@m.educaand.es>
2022-11-08 13:11:30 +0100

    dia 1 y 2 actualizados

---
+++ day2/variables.py
@@ -0,0 +1,16 @@
+#'Day 2: 30 Days of python programming'
+
+from operator import truediv
+
+
+first_name_var="Miguel"
+last_name_var="Luna"
+full_name_var="Miguel Luna"
+country_var="ESPAÑA"
+city_var="Jerez"
+age_var="16"
+year_var="2022"
+is_married=False
+is_true=True
+is_light_on=True
+print(country_var , city_var)
