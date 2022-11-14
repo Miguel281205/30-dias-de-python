d952f78ddb1a6b99a000422cc7a0bd7f0aaada3e
usuario <mlungut2812@m.educaand.es>
2022-11-14 12:39:05 +0100

    dias 1 y 2 actualizados

--- day2/variables.py
+++ day2/variables.py
@@ -1,7 +1,6 @@
-#'Day 2: 30 Days of python programming'
-
+#'Day 2: 30 Days of python program
+from cProfile import run
 from operator import truediv
-
 
 first_name_var="Miguel"
 last_name_var="Luna"
@@ -18,3 +17,29 @@ print(country_var , city_var)
 print(type(full_name_var))
 print(len(full_name_var))
 print(len(last_name_var))
+
+num_one=5
+num_two=4
+
+total=num_one+num_two
+print(total)
+
+diff=num_two-num_one
+print(diff)
+
+multiply=num_one*num_two
+print(multiply)
+
+divide=num_one/num_two
+print(divide)
+
+r=30
+pi=3,14
+diametro=30*2
+areaofcircle=pi*r
+circunferenciacirculo=pi*diametro
+
+print(areaofcircle,circunferenciacirculo)
+
+run 
+help("keywords")
